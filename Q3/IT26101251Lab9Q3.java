957688e4506088ca5c6eadb2ec9d6cf4c36e0851
IT26101251 <it26101251@my.sliit.lk>
2026-09-23 11:33:21 +0530

    Add IT26101251Lab9Q3.java to Q3

---
+++ Q3/IT26101251Lab9Q3.java
@@ -0,0 +1,28 @@
+public class IT26101251Lab9Q3 {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        
+        int result1 = square(add(multiply(3, 4), multiply(5, 7)));
+
+        
+        int term1 = square(add(4, 7));
+        int term2 = square(add(8, 3));
+        int result2 = add(term1, term2);
+
+        
+        System.out.println("Result of (3 * 4 + 5 * 7)^2     : " + result1);
+        System.out.println("Result of (4 + 7)^2 + (8 + 3)^2  : " + result2);
+    }
+}
